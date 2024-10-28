f07fbefad812da6de943164b505b85257f080812
Bonansea Mariano <marianobonanseapetrovial@gmail.com>
2024-10-28 12:05:36 -0300

    Tp5Ej3

---
+++ Tp5/Tp5Ej3/src/main/java/models/Reader.java
@@ -0,0 +1,31 @@
+package models;
+
+public class Reader implements Runnable {
+    private final SharedResource resource;
+    private final ReadWriteLock lock;
+    private String name;
+
+    public Reader(SharedResource resource, ReadWriteLock lock, int i) {
+        this.resource = resource;
+        this.lock = lock;
+        this.name = "Reader " + i;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                lock.lockRead();
+                long time = (long) (3000 + Math.random() * 2000);
+                System.out.println(name + " locks read for " + time + " ms");
+                int value = resource.getValue();
+                System.out.println(name + " Reads " + value);
+                System.out.println(name + " unlocks read");
+                lock.unlockRead();
+                Thread.sleep(time);
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
